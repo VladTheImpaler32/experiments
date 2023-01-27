ea43375d82ab5e31c2d588516d3f96735fc8140d
Ambrose <80496870+VladTheImpaler32@users.noreply.github.com>
2023-01-26 19:37:47 -0800

    Add files via upload

--- p5.js/Boxes/sketch.js
+++ p5.js/Boxes/sketch.js
@@ -1,48 +1,49 @@
 let scl = 50;
-let cols, rows;
+let cols, rows, toggle;
 let inc = 10;
 let zOff = 0;
 let clr = 0;
 
 function setup() {
   createCanvas(500, 800);
-  cols = floor(width / scl-1);
-  rows = floor(height / scl-1);
-  checkbox = createCheckbox('Color', false);
-  checkbox.changed(chng);
+  cols = floor(width / scl - 1);
+  rows = floor(height / scl - 1);
+  button = createButton("Toggle Color", true);
+  button.mousePressed(chng);
 }
 
 function chng() {
-  if (checkbox.checked()) {
+  if (!toggle) {
     clr = 1.1;
     loop();
   } else {
     clr = 0;
     loop();
   }
+  toggle = !toggle;
 }
 
 function draw() {
   background(0);
   xOff = 0;
-  for (let x=1; x<cols; x++){
+  for (let x = 1; x < cols; x++) {
     yOff = 0;
-    for (let y=1; y<rows; y++){
+    for (let y = 1; y < rows; y++) {
       let n = noise(xOff, yOff, zOff) * 1.5;
       let flip = random() < 0.5 ? -1 : 1;
       push();
-      translate(x*scl, y*scl);
-      rotate(y*y/200*n*flip);
+      translate(x * scl, y * scl);
+      rotate(((y * y) / 200) * n * flip);
       noFill();
-      colorMode(HSL,clr)
-      stroke(n,1,0.7)
-      quad(0,0, scl,0, scl,scl, 0,scl);
+      colorMode(HSL, clr);
+      stroke(n, 1, 0.7);
+      quad(0, 0, scl, 0, scl, scl, 0, scl);
       pop();
-      
+
       yOff += inc;
     }
     xOff += inc;
   }
-  zOff += 0.01
+  zOff += 0.01;
   noLoop();
 }
